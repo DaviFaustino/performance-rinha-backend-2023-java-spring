43ce082b3a4ccb85bc9be623ff7247700e3e2a6b
DaviFaustino <davifaustino.dev@gmail.com>
2024-11-19 23:13:53 -0300

    Add context test

---
+++ src/test/java/com/davifaustino/performancerinhabackend/PerformancerinhabackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.davifaustino.performancerinhabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+class PerformancerinhabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
