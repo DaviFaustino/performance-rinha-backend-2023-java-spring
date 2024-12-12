aea7b0d3152b7da9937500582c986af99d944ad7
DaviFaustino <davifaustino.dev@gmail.com>
2024-12-12 14:35:33 -0300

    feat: create getOnePessoa endpoint in PessoaController

--- src/main/java/com/davifaustino/performancerinhabackend/api/PessoaController.java
+++ src/main/java/com/davifaustino/performancerinhabackend/api/PessoaController.java
@@ -3,8 +3,11 @@ package com.davifaustino.performancerinhabackend.api;
 import java.net.URI;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +29,11 @@ public class PessoaController {
     public Mono<ResponseEntity<UUID>> savePessoa(@RequestBody PessoaDto pessoaDto) {
         return pessoaService.savePessoa(pessoaDto)
                             .map(uuid -> ResponseEntity.created(URI.create("/pessoas/" + uuid.toString())).body(uuid));
+    }
+
+    @GetMapping(value = "/pessoas/{id}")
+    public Mono<ResponseEntity<PessoaDto>> getOnePessoa(@PathVariable(value = "id") String id) {
+        return pessoaService.getOnePessoa(id)
+                            .map(pessoaDto -> ResponseEntity.status(HttpStatus.OK).body(pessoaDto));
     }
 }
