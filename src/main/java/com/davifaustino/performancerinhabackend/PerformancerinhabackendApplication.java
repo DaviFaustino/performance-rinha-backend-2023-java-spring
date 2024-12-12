4fcece30bd1f2c5613906f8ca4e14170ed86d662
DaviFaustino <davifaustino.dev@gmail.com>
2024-12-12 11:43:03 -0300

    feat: add maven structure

---
+++ src/main/java/com/davifaustino/performancerinhabackend/PerformancerinhabackendApplication.java
@@ -0,0 +1,13 @@
+package com.davifaustino.performancerinhabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformancerinhabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformancerinhabackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davifaustino/performancerinhabackend/PerformancerinhabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davifaustino.performancerinhabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformancerinhabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
